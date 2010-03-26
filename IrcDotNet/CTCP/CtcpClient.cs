e3839f7acf9f2af6a3a73f839f33cd5e1f351dff
Alex Regueiro <alexreg@gmail.com>
2010-03-26 15:09:51 +0000

    Factored out code from IrcClient for finding all message processors (methods marked with specific delegate) to ReflectionUtilities class. CtcpClient can now also use ReflectionUtilities class for message processors.

--- IrcDotNet/CTCP/CtcpClient.cs
+++ IrcDotNet/CTCP/CtcpClient.cs
@@ -24,8 +24,6 @@ namespace IrcDotNet.CTCP
             
             this.ircClient.Connected += ircClient_Connected;
             this.ircClient.Disconnected += ircClient_Disconnected;
-            this.ircClient.LocalUser.JoinedChannel += ircClient_LocalUser_JoinedChannel;
-            this.ircClient.LocalUser.LeftChannel += ircClient_LocalUser_LeftChannel;
         }
 
         /// <summary>
@@ -39,32 +37,22 @@ namespace IrcDotNet.CTCP
 
         private void ircClient_Connected(object sender, EventArgs e)
         {
-            // TODO
+            this.ircClient.LocalUser.PreviewMessageReceived += ircClient_LocalUser_PreviewMessageReceived;
+            this.ircClient.LocalUser.PreviewNoticeReceived += ircClient_LocalUser_PreviewNoticeReceived;
         }
 
         private void ircClient_Disconnected(object sender, EventArgs e)
         {
-            // TODO
+            this.ircClient.LocalUser.PreviewMessageReceived -= ircClient_LocalUser_PreviewMessageReceived;
+            this.ircClient.LocalUser.PreviewNoticeReceived -= ircClient_LocalUser_PreviewNoticeReceived;
         }
 
-        private void ircClient_LocalUser_JoinedChannel(object sender, IrcChannelEventArgs e)
-        {
-            e.Channel.PreviewMessageReceived += client_Channel_PreviewMessageReceived;
-            e.Channel.PreviewNoticeReceived += client_Channel_PreviewNoticeReceived;
-        }
-
-        private void ircClient_LocalUser_LeftChannel(object sender, IrcChannelEventArgs e)
-        {
-            e.Channel.PreviewMessageReceived -= client_Channel_PreviewMessageReceived;
-            e.Channel.PreviewNoticeReceived -= client_Channel_PreviewNoticeReceived;
-        }
-
-        private void client_Channel_PreviewMessageReceived(object sender, IrcPreviewMessageEventArgs e)
+        private void ircClient_LocalUser_PreviewMessageReceived(object sender, IrcPreviewMessageEventArgs e)
         {
             // TODO
         }
 
-        private void client_Channel_PreviewNoticeReceived(object sender, IrcPreviewMessageEventArgs e)
+        private void ircClient_LocalUser_PreviewNoticeReceived(object sender, IrcPreviewMessageEventArgs e)
         {
             // TODO
         }
---
+++ IrcDotNet/ReflectionUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace IrcDotNet
+{
+    internal static class ReflectionUtilities
+    {
+        public static IEnumerable<Tuple<TAttribute, TDelegate>> GetMethodAttributes<TAttribute, TDelegate>(
+            this object obj)
+            where TAttribute : Attribute
+            where TDelegate : class
+        {
+            // Find all methods in class that are marked by one or more instances of TAttribute.
+            // Add each pair of attribute instance & method delegate to dictionary.
+            var messageProcessorsMethods = obj.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (var methodInfo in messageProcessorsMethods)
+            {
+                var messageProcessorAttributes = (TAttribute[])methodInfo.GetCustomAttributes(
+                    typeof(TAttribute), true);
+                if (messageProcessorAttributes.Length > 0)
+                {
+                    var methodDelegate = (TDelegate)(object)Delegate.CreateDelegate(typeof(TDelegate), obj, methodInfo);
+                    foreach (var attribute in messageProcessorAttributes)
+                        yield return Tuple.Create(attribute, methodDelegate);
+                }
+            }
+        }
+    }
+}
