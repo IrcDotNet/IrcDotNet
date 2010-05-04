9455a1ca287c5d61d917eae158b80816e84d5c74
Alex Regueiro <alexreg@gmail.com>
2010-05-04 22:37:51 +0100

    Completed CTCP support (based in CtcpClient class), compliant with current de facto standard of transmitting tagged data. Written unit tests for CTCP PING and VERSION requests and responses. Minor bug fixes and refactoring. Small improvements to XML comments.

--- IrcDotNet/CTCP/CtcpClientMessageProcessing.cs
+++ IrcDotNet/CTCP/CtcpClientMessageProcessing.cs
@@ -1,13 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
-namespace IrcDotNet
+namespace IrcDotNet.Ctcp
 {
     // Defines all message processors for the client.
     partial class CtcpClient
     {
-        //
+        /// <summary>
+        /// Process PING messages from a user.
+        /// </summary>
+        /// <param name="message">The message received from the server.</param>
+        [MessageProcessor("ping")]
+        protected void ProcessMessagePing(CtcpMessage message)
+        {
+            Debug.Assert(message.Data != null);
+
+            if (message.IsResponse)
+            {
+                // Calculate time elapsed since the ping request was sent.
+                var sendTime = new DateTime(long.Parse(message.Data));
+                var receiveTime = DateTime.Now;
+                var pingTime = receiveTime - sendTime;
+
+                OnPingResponseReceived(new CtcpPingResponseReceivedEventArgs(message.Source, pingTime));
+            }
+            else
+            {
+                SendMessagePing(message.Source, message.Data, true);
+            }
+        }
+
+        /// <summary>
+        /// Process VERSION messages from a user.
+        /// </summary>
+        /// <param name="message">The message received from the server.</param>
+        [MessageProcessor("version")]
+        protected void ProcessMessageVersion(CtcpMessage message)
+        {
+            if (message.IsResponse)
+            {
+                var versionInfo = message.Data;
+
+                OnVersionResponseReceived(new CtcpVersionResponseReceivedEventArgs(message.Source, versionInfo));
+            }
+            else
+            {
+                if (this.ClientVersion != null)
+                {
+                    SendMessageVersion(message.Source, this.ClientVersion);
+                }
+            }
+        }
     }
 }
--- IrcDotNet/TextUtilities.cs
+++ IrcDotNet/TextUtilities.cs
@@ -15,6 +15,54 @@ namespace IrcDotNet
             return match.Value;
         }
 
+        public static string Quote(this string value, char escapeChar, IDictionary<char, char> quotedChars)
+        {
+            var textBuilder = new StringBuilder(value.Length * 2);
+            for (int i = 0; i < value.Length; i++)
+            {
+                char curQuotedChar = escapeChar;
+                if (quotedChars.TryGetValue(value[i], out curQuotedChar) || value[i] == escapeChar)
+                {
+                    textBuilder.Append(escapeChar);
+                    textBuilder.Append(curQuotedChar);
+                }
+                else
+                {
+                    textBuilder.Append(value[i]);
+                }
+            }
+
+            return textBuilder.ToString();
+        }
+
+        public static string Dequote(this string value, char escapeChar, IDictionary<char, char> dequotedChars)
+        {
+            var textBuilder = new StringBuilder(value.Length);
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == escapeChar)
+                {
+                    i++;
+                    char curDequotedChar = escapeChar;
+                    if (dequotedChars.TryGetValue(value[i], out curDequotedChar) || value[i] == escapeChar)
+                    {
+                        textBuilder.Append(curDequotedChar);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException(
+                            Properties.Resources.ErrorMessageInvalidQuotedChar);
+                    }
+                }
+                else
+                {
+                    textBuilder.Append(value[i]);
+                }
+            }
+
+            return textBuilder.ToString();
+        }
+
         public static Tuple<string, string> SplitAtIndex(this string value, int index)
         {
             if (index < 0)
