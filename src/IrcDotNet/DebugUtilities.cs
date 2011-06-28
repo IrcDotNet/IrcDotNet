009207ee70c841992b2cca8d86e3d255fcaeac1c
Alex Regueiro <alexreg@gmail.com>
2011-06-28 03:18:06 +0100

    SocketAsyncEventArgs objects are now disposed correctly by IrcClient when finished with. Fixed bug where zero-length password raised low-level protocol error on sending. Library project now builds without errors in Release mode. Updated Silverlight library project so that it now builds again. Updated AUTHORS file and fixed Markdown syntax.

--- src/IrcDotNet/DebugUtilities.cs
+++ src/IrcDotNet/DebugUtilities.cs
@@ -10,11 +10,15 @@ namespace IrcDotNet
     // TODO: use TraceSource here and configure trace listeners in test project.
     internal static class DebugUtilities
     {
+        [Conditional("DEBUG")]
         public static void WriteIrcRawLine(IrcClient client, string line)
         {
+#if DEBUG
             WriteEvent("({0}) {1}", client.ClientId, line);
+#endif
         }
 
+        [Conditional("DEBUG")]
         public static void WriteEvent(string message, params object[] args)
         {
             Debug.WriteLine(string.Format("{0:HH:mm:ss} {1}", DateTime.Now, string.Format(message, args)));
