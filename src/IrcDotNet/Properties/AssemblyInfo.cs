f5b2bd40b297ad80984e01ef488521a9bbadb504
Alex Regueiro <alexreg@gmail.com>
2011-07-01 00:27:42 +0100

    Removed Silverlight solution and added Silverlight library project to main solution. Added NuSpec definitions for for NuGet packages (library and samples) in root pkg/ directory. Minor fixes to README files.

--- src/IrcDotNet/Properties/AssemblyInfo.cs
+++ src/IrcDotNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IRC.NET Library")]
-[assembly: AssemblyDescription("Library for communicating via Internet Relay Chat (IRC) protocol and its extensions.")]
+[assembly: AssemblyDescription("Library for communicating via Internet Relay Chat (IRC) protocol and its extensions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IRC.NET")]
