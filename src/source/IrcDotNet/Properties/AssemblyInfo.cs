ffcb53f5871a87650982da995db12ca7d86fb135
Matthias Dittrich <matthi.d@googlemail.com>
2015-01-17 17:31:16 +0100

    use the generated assembly info and remove version info from the normal assembly info file.

--- src/source/IrcDotNet/Properties/AssemblyInfo.cs
+++ src/source/IrcDotNet/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IRC.NET Library")]
 [assembly: AssemblyDescription("Library for communicating via Internet Relay Chat (IRC) protocol and its extensions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IRC.NET")]
-[assembly: AssemblyCopyright("Copyright © Alex Regueiro 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +19,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("67fa7d1e-5e04-4af5-93ec-f57d0183be2b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.*")]
-//[assembly: AssemblyFileVersion("0.1")]
