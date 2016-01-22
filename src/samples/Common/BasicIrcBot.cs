b60a2eb42cdc723e1924e5502e4789007c4ce193
izrik <izrik@izrik.com>
2016-01-22 15:13:39 -0600

    Modify the 'connect' command to take optional useSsl and port parameters.

--- src/samples/Common/BasicIrcBot.cs
+++ src/samples/Common/BasicIrcBot.cs
@@ -66,7 +66,19 @@ namespace IrcDotNet.Samples.Common
             if (parameters.Count < 1)
                 throw new ArgumentException(Properties.Resources.MessageNotEnoughArgs);
 
-            Connect(parameters[0], this.RegistrationInfo);
+            bool useSsl = false;
+            if (parameters.Count > 1)
+            {
+                bool.TryParse(parameters[1], out useSsl);
+            }
+
+            int port = 6697;
+            if (parameters.Count > 2)
+            {
+                int.TryParse(parameters[2], out port);
+            }
+
+            Connect(parameters[0], this.RegistrationInfo, port, useSsl);
         }
 
         private void ProcessCommandDisconnect(string command, IList<string> parameters)
